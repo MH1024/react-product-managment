5b8361fe7aa8e320eadd62a1dc2299dedb658750
Sam <huangsaiy@hotmail.com>
2020-10-04 01:12:03 +1000

    [init]add router and add navbar for app

---
+++ react-app/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper gret darken-3">
+            <div className="container">
+                <Link to='/' className='brand-logo'>Products</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
