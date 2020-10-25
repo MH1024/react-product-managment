4fdda9ed8872cd9fb2a2825e909f6d0adb115e66
Sam <huangsaiy@hotmail.com>
2020-10-25 23:23:15 +1100

    [WIP]add signedin nav component

--- react-app/src/components/layout/Navbar.js
+++ react-app/src/components/layout/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import SignedInLinks from './SignedInLinks'
 const Navbar = () => {
     return (
         <nav className="nav-wrapper gret darken-3">
             <div className="container">
                 <Link to='/' className='brand-logo'>Products</Link>
+                <SignedInLinks />
             </div>
         </nav>
     )
