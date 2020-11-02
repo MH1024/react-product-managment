51e1a51399144c00520bd0bec3846c912245cfe5
Sam <huangsaiy@hotmail.com>
2020-11-02 23:59:11 +1100

    [WIP]add product list component

---
+++ react-app/src/components/products/ProductsList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductsList = () => {
+    return (
+        <div className="prod-list section">
+
+            <div className="card z-depth-0 prod-summary">
+                <div className="card-conecnt grey-text text-darken-3">
+                    <span className="card-title">Product Title</span>
+                    <p>Create by K H</p>
+                    <p className="grey-text">3 Sep 2020</p>
+                </div>
+            </div>
+        </div>
+    )
+}
