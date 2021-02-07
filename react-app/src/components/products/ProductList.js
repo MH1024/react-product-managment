e8fffbde39d10f3133f08edd7de5f8211ae8d261
Sam <huangsaiy@hotmail.com>
2021-02-08 00:39:18 +1100

    [refactor]module product summary at list.

--- react-app/src/components/products/ProductList.js
+++ react-app/src/components/products/ProductList.js
@@ -1,16 +1,10 @@
 import React from 'react'
-
+import ProductSummary from './ProductSummary';
 const ProductList = () => {
     return (
         <div className="prod-list section">
-
-            <div className="card z-depth-0 prod-summary">
-                <div className="card-conecnt grey-text text-darken-3">
-                    <span className="card-title">Product Title</span>
-                    <p>Create by K H</p>
-                    <p className="grey-text">3rd Sep 2020</p>
-                </div>
-            </div>
+                <ProductSummary />
+                <ProductSummary />
         </div>
     )
 }
---
+++ react-app/src/components/products/ProductSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductSummary = () => {
+    return (
+        <div className="card z-depth-0 product-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Product Title: Card 1</span>
+                <p>Post by the K Huang</p>
+                <p className="grey-text">Date: 08/02/2021</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProductSummary
